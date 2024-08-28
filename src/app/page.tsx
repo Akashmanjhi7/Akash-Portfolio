b330f4b144dcbbacbd4746361a24fb4c4767258f
akash <akashmanjhi1111198@gmail.com>
2024-08-28 14:23:10 +0530

    fix page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,38 @@
 'use client'
-import Footer from "@/components/Footer"
-import Home from "@/components/Home"
-import Navbar from "@/components/Navbar"
-import Photos from "@/components/Photos"
-import PlayReel from "@/components/PlayReel"
-import Spread from "@/components/Spread"
-import Work from "@/components/Work"
-import LocomotiveScroll from 'locomotive-scroll'
+import { useEffect } from 'react';
+import LocomotiveScroll from 'locomotive-scroll';
+import Footer from '@/components/Footer';
+import Home from '@/components/Home';
+import Navbar from '@/components/Navbar';
+import Photos from '@/components/Photos';
+import PlayReel from '@/components/PlayReel';
+import Spread from '@/components/Spread';
+import Work from '@/components/Work';
 
-const page = () => {
+const Page = () => {
 
-const locomotiveScroll = new LocomotiveScroll();
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('.scroll-container'),
+      smooth: true,
+    });
+
+    return () => {
+      scroll.destroy(); // Cleanup on component unmount
+    };
+  }, []);
 
   return (
-    <div className="overflow-x-hidden ">
-      <Navbar/>
-      <Home/>
-      <Work/>
-      <PlayReel/> 
-    <Photos/>
-    <Spread/>
-    <Footer/>
+    <div className="overflow-x-hidden scroll-container">
+      <Navbar />
+      <Home />
+      <Work />
+      <PlayReel /> 
+      <Photos />
+      <Spread />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
